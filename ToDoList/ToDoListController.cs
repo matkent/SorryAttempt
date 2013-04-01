fca1472e7c82d02f4a1e7660c6f31354d89d8df4
unknown <mat.kent@Mat-Dev-PC.imaginelearning.local>
2013-04-01 11:41:14 -0600

    Added some static stuff

--- ToDoList/ToDoListController.cs
+++ ToDoList/ToDoListController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using FubuMVC.Core;
 using FubuMVC.Core.Continuations;
 
 namespace ToDoList
@@ -11,18 +12,22 @@ namespace ToDoList
 		private int _lastId;
 		private ToDoListViewModel _model;
 
-		public static string SessionKey = "ToDoListSession";
 
 		public ToDoListViewModel ToDoList()
 		{
-			_model = (ToDoListViewModel)HttpContext.Current.Session[SessionKey] ?? new ToDoListViewModel();
+			_model = (ToDoListViewModel)HttpContext.Current.Session[ToDoModel.SessionKey] ?? new ToDoListViewModel();
 
 			return _model;
 		}
 
-		public void post_addToDoWithDescription_Description(ToDoModel toDo)
+		[UrlPattern("addToDo/{Description}")]
+		public FubuContinuation AddToDo(ToDoModel input)
 		{
-			_model.ToDos.Add(new ToDoModel { Id = GetId(), Description = toDo.Description });
+			_model = (ToDoListViewModel)HttpContext.Current.Session[ToDoModel.SessionKey];
+			var toStore = new ToDoModel { Description = input.Description };
+			_model.ToDos.Add(toStore);
+			HttpContext.Current.Session[ToDoModel.SessionKey] = _model;
+			return FubuContinuation.RedirectTo<ToDoListController>(x => x.ToDoList());
 		}
 
 		public void get_removeToDoWithId_Id(ToDoModel toDo)
@@ -49,6 +54,7 @@ namespace ToDoList
 
 	public class ToDoModel
 	{
+		public static string SessionKey = "ToDoListSession";
 		public int Id { get; set; }
 		public string Description { get; set; }
 	}
